826c930d2c9defd46976fb57bca46495a67ca675
aysenurkaplan <kaysenur551@gmail.com>
2023-06-14 09:37:39 +0300

    ilan oluşturma ve basvuruyu değ. işl. başlandı.

---
+++ resources/views/admin/ilan/create.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">Duyuru Oluştur </x-slot>
+
+    <div class="card">
+        <div class="card-body">
+            <form method="POST"  action="{{route('ilans.store')}}">
+                @csrf
+                <div class="form-group">
+                    <label>İlan Tarihi</label>
+                    <input type="datetime-local" name="tarih" class="form-control" >
+                </div>
+                <div class="form-group">
+                    <label>İlan Başlığı</label>
+                    <input type="text" name="baslık" class="form-control" value="{{old('baslık')}}" >
+                </div>
+                <div class="form-group">
+                    <label>İlan Metni</label>
+                    <textarea name="description" class="form-control" rows="5" >{{old('description')}}</textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success btn-block" >Duyuru Oluştur</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</x-app-layout>
